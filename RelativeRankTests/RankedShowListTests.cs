a4a2df51199a1c907f38e492fc44b846ae248359
Martin <martinpayne73@gmail.com>
2019-06-18 22:36:48 -0400

    added tests for indexers and auto sorting of RankedShowList and got them to pass

--- RelativeRankTests/RankedShowListTests.cs
+++ RelativeRankTests/RankedShowListTests.cs
@@ -1,6 +1,7 @@
 using Xunit;
 using RelativeRank.Entities;
 using RelativeRank.Models;
+using System.Collections.Generic;
 
 namespace RelativeRankTests
 {
@@ -16,6 +17,60 @@ namespace RelativeRankTests
             var showsInListAfterAddingShow = showList.ShowsInList;
 
             Assert.Equal(showsInListBeforeAddingShow + 1, showsInListAfterAddingShow);
+        }
+
+        [Fact]
+        public void RankOfEachShowInRankedShowListShouldMatchIndexInShowListPlusOne()
+        {
+            var showList = new RankedShowList();
+
+            var numberOfShowsToTest = 3;
+
+            for (var i = 0; i < numberOfShowsToTest; i++)
+            {
+                showList.Add(new RankedShow());
+            }
+
+            for (var i = 0; i < numberOfShowsToTest; i++)
+            {
+                Assert.Equal(i + 1, showList[i].Rank);
+            }
+        }
+
+        [Fact]
+        public void AddingShowWithRankOneShouldIncrementAllOtherShowsRankByOne()
+        {
+            var showList = new RankedShowList();
+            var rankedShows = new List<RankedShow>();
+
+            var numberOfShowsToTest = 3;
+            for (var i = 0; i < numberOfShowsToTest; i++)
+            {
+                showList.Add(new RankedShow() { Name = $"{i}" });
+
+                //getting a reference of each show in the internal list
+                rankedShows.Add(showList[i]);
+            }
+
+            var startingRanks = new List<short>();
+            for (var i = 0; i < numberOfShowsToTest; i++)
+            {
+                startingRanks.Add(rankedShows[i].Rank);
+            }
+
+            //check before adding to showList that each show rank is it's expected value
+            foreach (var show in rankedShows)
+            {
+                Assert.Equal(show.Name, $"{show.Rank - 1}");
+            }
+
+            showList.Add(new RankedShow() { Rank = 1 });
+
+            //check after adding show with rank 1 asserting each show rank increased by one
+            foreach (var show in rankedShows)
+            {
+                Assert.Equal(show.Name, $"{show.Rank - 2}");
+            }
         }
     }
 }
--- api/Entities/RankedShowList.cs
+++ api/Entities/RankedShowList.cs
@@ -14,11 +14,35 @@ namespace RelativeRank.Entities
         {
             _backingList = new List<RankedShow>();
         }
+
+        public RankedShow this[int i]
+        {
+            get { return _backingList[i]; }
+            set { _backingList[i] = value; }
+        }
         public short ShowsInList { get { return (short) _backingList.Count; } }
 
         public void Add(RankedShow show)
         {
-            _backingList.Add(show);
+            if (show.Rank <= 0)
+            {
+                show.Rank = (short)(_backingList.Count + 1);
+                _backingList.Add(show);
+            }
+            else
+            {
+                _backingList.Add(show);
+                var index = _backingList.Count - 2;
+                while (index >= 0 && _backingList[index].Rank >= show.Rank)
+                {
+                    _backingList[index + 1] = _backingList[index];
+                    _backingList[index + 1].Rank++;
+                    index--;
+                }
+
+                _backingList[index + 1] = show;
+            }
+
         }
     }
 }
