4ab3f11016a319fd8bcc72bb800a1e6847dd5686
Martin <martinpayne73@gmail.com>
2020-04-13 12:59:01 -0400

    simplify update show list query

--- src/Data/UserShowListRepository.cs
+++ src/Data/UserShowListRepository.cs
@@ -38,65 +38,17 @@ namespace RelativeRank.Data
                 throw new ArgumentNullException(nameof(rankedShowList));
             }
 
-            var nameToShowTable = new Dictionary<string, RankedShow>();
-            foreach (var show in rankedShowList)
+            _context.RemoveRange(_context.UserToShowMapping.Where(userShow => userShow.UserId == userId));
+
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+
+            _context.AddRange(rankedShowList.Select(show => new UserToShowMapping
             {
-                nameToShowTable.Add(show.Name, show);
-            }
-
-            var showList = _context.UserToShowMapping.Where(userShow => userShow.UserId == userId)
-                .Join(
-                    _context.Show,
-                    userShow => userShow.ShowId,
-                    show => show.Id,
-                    (userShow, show) => new
-                    {
-                        Name = show.Name,
-                        Rank = userShow.Rank,
-                        ShowId = show.Id,
-                    }
-                );
-
-            var showNameToIdTable = new Dictionary<string, int>();
-            var showIdToNameTable = new Dictionary<int, string>();
-            foreach (var show in showList)
-            {
-                showNameToIdTable.Add(show.Name, show.ShowId);
-                showIdToNameTable.Add(show.ShowId, show.Name);
-            }
-
-            var showsToDelete = _context.UserToShowMapping.Where(userShow => userShow.UserId == userId)
-                .AsEnumerable()
-                .Where(show => !nameToShowTable.ContainsKey(showIdToNameTable[show.ShowId]));
-
-            _context.RemoveRange(showsToDelete);
-
-            foreach (var show in rankedShowList)
-            {
-                if (showNameToIdTable.ContainsKey(show.Name))
-                {
-                    var showToUpdate = _context.UserToShowMapping
-                        .Where(userShow => showNameToIdTable.ContainsKey(show.Name) &&
-                            userShow.ShowId == showNameToIdTable[show.Name] &&
-                            userShow.UserId == userId)
-                        .FirstOrDefault();
-
-                    showToUpdate.Rank = show.Rank;
-                    showToUpdate.PercentileRank = show.PercentileRank;
-                }
-                else
-                {
-                    var newShow = new UserToShowMapping
-                    {
-                        UserId = userId,
-                        ShowId = _context.Show.Where(s => s.Name == show.Name).First().Id,
-                        Rank = show.Rank,
-                        PercentileRank = show.PercentileRank
-                    };
-
-                    _context.Add(newShow);
-                }
-            }
+                UserId = userId,
+                ShowId = _context.Show.Where(s => s.Name == show.Name).First().Id,
+                Rank = show.Rank,
+                PercentileRank = show.PercentileRank
+            }));
 
             await _context.SaveChangesAsync().ConfigureAwait(false);
 
